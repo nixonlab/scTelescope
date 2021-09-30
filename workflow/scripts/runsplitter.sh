700dbf73ca50a31ebccf401847e3589879e94ac3
Zeldritch <santiago.sandoval.m@gmail.com>
2021-09-30 08:45:46 -0500

    fixed runscript

--- workflow/scripts/runsplitter.sh
+++ workflow/scripts/runsplitter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 conda activate telescope
-python cellsplit_sc.py --bam /efs/projects/align-pbmc/results/pbmc_10k_v3_fastqs/pbmc_10k_v3_GDC38.Aligned.sortedByCoord.out.bam --brcds /efs/projects/results/pbmc_10k_v3_fastqs/pbmc_10k_v3_GDC38.Aligned.sortedByCoord.out.bam --brcds /efs/projects/align-pbmc/results/pbmc_10k_v3_fastqs/pbmc_10k_v3_GDC38.Solo.out/Gene/filtered/barcodes.tsv --dest /fsx/users/santiago2/cellsplit_sc_results/ --log /fsx/users/santiago2/cellsplit_log.tsv 
+python cellsplit_sc.py --bam /efs/projects/align-pbmc/results/pbmc_10k_v3_fastqs/pbmc_10k_v3_GDC38.Aligned.sortedByCoord.out.bam --brcds /efs/projects/align-pbmc/results/pbmc_10k_v3_fastqs/pbmc_10k_v3_GDC38.Solo.out/Gene/filtered/barcodes.tsv --dest /fsx/users/santiago2/cellsplit_sc_results/ --log /fsx/users/santiago2/cellsplit_log.tsv
